a77beac0cabf6cbbcfe935c4ec4aefa08b570cc9
José Quintinno <credentium@outlook.com.br>
2025-12-02 14:24:31 -0300

    build: Gerar Versão da Aplicação (Docker) (Produção)

--- credentium.build.bat
+++ credentium.build.bat
@@ -1,6 +1,14 @@
+# DOCKER
+
 docker compose down
 
 docker stop f90ce909d9ca
 docker rm f90ce909d9ca
 
 docker compose up -d --build
+
+# RENDER
+
+CREDENTIUM_DATASOURCE_URL=postgresql://db_credentium_producao_user:YOUR_PASSWORD@dpg-d4n0b449c44c738d623g-a/db_credentium_producao
+CREDENTIUM_DATASOURCE_USERNAME=db_credentium_producao_user
+CREDENTIUM_DATASOURCE_PASSWORD=NLaeHzxwz3jftMHSBwRG4etWiyrV3MeH
